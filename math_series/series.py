1993f1ab6060d272115afcc5af6c4991f29c461d
Liesl White <lieslfwhite@gmail.com>
2022-08-03 18:03:50 -0700

    init.py added and first test with fibonacci passing

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,13 @@
-def fibonacci
+def fibonacci(n):
+    """
+    The Fibonacci sequence starts with integers 0 then 1 and continues with a list of numbers where the two previous integers are added together to create a third value. It occurs infiintely using recursion. This function will return the value at whatever given position (input for n)
+    """
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# def lucas
 
 
-def lucas
-
-
-def sum_series
+# def sum_series
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,5 @@
-def test_fibonacci():
-    actual =
-    expected =
-    assert acutal == expected
+from math_series.series import fibonacci
+
+
+def test_fibonacci_if_int():
+    assert type(fibonacci(3)) is int
